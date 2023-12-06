219ad47d49a0569cabdfbaf5738f050b655f7158
F-zeinali <fatemeh.zeinali7026@gmail.com>
2023-12-07 01:42:59 +0330

    add random images

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+//Random Background Images
+function randomImage() {
+  let images = [
+    "images/algodones_dunes_california-wallpaper-1920x1080.jpg",
+    "images/denmark_forest-wallpaper-1920x1080.jpg",
+    "images/field_nature_landscape-wallpaper-1920x1080.jpg",
+    "images/flower_field_3-wallpaper-1920x1080.jpg",
+    "images/flower_field_nature_landscape-wallpaper-1920x1080.jpg",
+    "images/landscape_42-wallpaper-1920x1080.jpg",
+    "images/tree_in_a_field_spring_background-wallpaper-1920x1080.jpg",
+    "images/tropical_beach_resort-wallpaper-1920x1080.jpg",
+    "images/white-christmas-in-winter-82n7x2vx82r8cj9n.jpg",
+  ];
+
+  let randomImgIndex;
+
+  randomImgIndex = Math.floor(Math.random() * images.length);
+  document.body.style.background = "url('" + images[randomImgIndex] + "')";
+  document.body.style.backgroundSize = "cover";
+}
+
+document.addEventListener("DOMContentLoaded", randomImage);
+
 //First - Way;
 let cityData = {
   tehran: {
